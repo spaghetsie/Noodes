f4dd3300bb1e16a14fae9b9f358ecc1dab78368b
spaghetsie <vachvojtech1212@gmail.com>
2021-06-27 18:20:44 +0200

    org creation working

--- Assets/scripts/CreateOrganism.cs
+++ Assets/scripts/CreateOrganism.cs
@@ -1,25 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System.Linq;
 
 public class CreateOrganism : MonoBehaviour
 {
+    public class Muscle
+    {
+        public int id;
+        public SpringJoint2D muscle;
+
+        public Muscle(int Id){
+            this.id = Id;
+        }
+    }
+
     public class Cell
     {
+        public int id;
         public Rigidbody2D rb;
-        public SpringJoint2D[] muscles;
+        public Org body;
+        public Muscle[] muscles;
 
-        public Cell(string Type, int connections, Transform org, GameObject prefab){
-            
+        public Cell(Org Body, int outputCount, int muscleCount, int connections, Transform org, GameObject prefab){
+            this.id = outputCount;
+            this.muscles = new Muscle[connections];
+            this.body = Body;
             GameObject cell = Instantiate(prefab);
+            Vector2 spawnPos = new Vector2(Random.Range(-0.1f, 0.1f), Random.Range(-0.1f, 0.1f));
+            spawnPos.x += org.position.x;
+            spawnPos.y += org.position.y;
+            cell.transform.position = spawnPos;
             cell.SetActive(true);
             cell.transform.parent = org;
-            muscles = new SpringJoint2D[connections];
-            rb = cell.GetComponent<Rigidbody2D>();
-            for(int i=0; i< connections; i++){
-                muscles[i] = cell.AddComponent<SpringJoint2D>();
-                muscles[i].autoConfigureDistance = false;
-                muscles[i].distance = 1;
+
+
+            this.rb = cell.GetComponent<Rigidbody2D>();
+            for(int i=0; i<connections; i++){
+                Body.muscles[muscleCount+i] = new Muscle(outputCount+i+1);
+                this.muscles[i] = Body.muscles[muscleCount+i];
+                Body.muscles[muscleCount+i].muscle = cell.AddComponent<SpringJoint2D>();
+                Body.muscles[muscleCount+i].muscle.autoConfigureDistance = false;
+                Body.muscles[muscleCount+i].muscle.distance = 5;
+                Body.muscles[muscleCount+i].muscle.enableCollision = true;
             }
             
         }
@@ -28,44 +51,52 @@ public class CreateOrganism : MonoBehaviour
     public class Nnet
     {
         public float[][] nodes;
-        float[][,] weights;
+        public float[][,] weights;
 
         public Nnet(int Inputs, int HLcount, int HLwidth, int Outputs){
-            nodes = new float[HLcount+2][];
-            weights = new float[HLcount+1][,];
+            this.nodes = new float[HLcount+2][];
+            this.weights = new float[HLcount+1][,];
 
-            nodes[0] = new float[Inputs];
-            weights[0] = new float[HLwidth, Inputs];
+            this.nodes[0] = new float[Inputs];
+            this.weights[0] = new float[HLwidth, Inputs+1];
 
             for (int i = 0; i<HLcount; i++){
-                nodes[i+1] = new float[HLwidth];
+                this.nodes[i+1] = new float[HLwidth];
             }
             for (int i = 0; i<HLcount-1; i++){
-                weights[i+2] = new float[HLwidth, HLwidth];
+                this.weights[i+2] = new float[HLwidth, HLwidth+1];
             }
 
-            nodes[HLcount+1] = new float[Outputs];
-            weights[HLcount] = new float[Outputs, HLwidth];
-            }
+            this.nodes[HLcount+1] = new float[Outputs];
+            this.weights[HLcount] = new float[Outputs, HLwidth+1];
+        }
 
-        void UpdateNodes()
+        public Nnet(Nnet nnet){
+            this.nodes = nnet.nodes;
+            this.weights = nnet.weights;
+        }
+
+        public void UpdateNodes()
         {
-            for(int l=1; l < nodes.GetLength(0); l++){
-                for(int n=0; n<nodes[l].GetLength(0); n++){
-                    nodes[l][n] = 0f;
-                    for(int c=0; c<weights[l-1].GetLength(1); c++){
-                        nodes[l][n] += weights[l-1][n, c] * nodes[l-1][c];
+            for(int l=1; l < this.nodes.GetLength(0); l++){
+                for(int n=0; n<this.nodes[l].GetLength(0); n++){
+                    this.nodes[l][n] = 0f;
+                    for(int c=0; c<this.weights[l-1].GetLength(1)-1; c++){
+                        this.nodes[l][n] += this.weights[l-1][n, c] * this.nodes[l-1][c];
                     }
+                    int lastIndex = weights[l-1].GetLength(1)-1;
+                    this.nodes[l][n] = (float)System.Math.Tanh(this.nodes[l][n]);
+                    this.nodes[l][n] += this.weights[l-1][n, lastIndex];
                 }
             }
         }
         
-        void MutateWeights(int chance=5, float range=0.05f){
-            for(int l=1; l < nodes.GetLength(0); l++){
-                for(int n=0; n<nodes[l].GetLength(0); n++){
-                    for(int c=0; c<weights[l-1].GetLength(1); c++){
+        public void MutateWeights(int chance=5, float range=0.05f){
+            for(int l=1; l < this.nodes.GetLength(0); l++){
+                for(int n=0; n<this.nodes[l].GetLength(0); n++){
+                    for(int c=0; c<this.weights[l-1].GetLength(1); c++){
                         if(Random.Range(0, 101) <= chance)
-                            weights[l-1][n, c] += Random.Range(-range, range);
+                            this.weights[l-1][n, c] += Random.Range(-range, range);
                     }
                 }
             }
@@ -74,37 +105,124 @@ public class CreateOrganism : MonoBehaviour
 
     public class Org
     {
-        Cell[] cells;
+        public Cell[] cells;
+        public Muscle[] muscles;
         int[,] connections;
-        Nnet nnet;
+        public Nnet nnet;
         string name;
+        public GameObject body;
+        int outputs = 0;
+        int Mcount = 0;
         public Org(int CellCount, int[][] Connections, Nnet Nnet, string Name){
-            name = Name;
-            GameObject org = new GameObject(name);
+            this.name = Name;
+            this.body = new GameObject(name);
+            int muscleCount = 0;
+            for(int i = 0; i<Connections.Length; i++){
+                muscleCount += Connections[i].Length;
+            }
+            this.muscles = new Muscle[muscleCount];
 
-            cells = new Cell[CellCount];
+            this.cells = new Cell[CellCount];
             GameObject NodePrefab = GameObject.Find("prefabs").transform.GetChild(1).gameObject;
             for(int i=0;i<CellCount;i++){
-                cells[i] = new Cell("basic", Connections[i].Length, org.transform, NodePrefab);
+                this.cells[i] = new Cell(this, this.outputs, this.Mcount, Connections[i].Length, this.body.transform, NodePrefab);
+                this.outputs += Connections[i].Length+1;
+                this.Mcount += Connections[i].Length;
             }
+            if(this.outputs != this.cells.Length+muscles.Length)
+                Debug.Log("something fucked up a lot");
             for(int o = 0; o<Connections.Length; o++){
-                for(int c=0; c<Connections[o].Length; c++){
-                    cells[o].muscles[c].connectedBody = cells[Connections[o][c]].rb;
-                }
+                for(int c = 0; c< Connections[o].Length; c++)
+                    {
+                        this.cells[o].muscles[c].muscle.connectedBody = this.GetCell(Connections[o][c]).rb;
+                    }
+                    
             }
 
-            nnet = Nnet;
+            this.nnet = Nnet;
+        }
+        
+        public Cell GetCell(int Id)
+        {
+            Cell cell = null;
+            for(int i = 0; i<this.cells.Length; i++){
+                if(this.cells[i].id == Id)
+                    cell = this.cells[i];
+            }
+            return cell;
+        }
+
+        public Muscle GetMuscle(int Id)
+        {
+            Muscle muscle = null;
+            for(int i = 0; i<this.muscles.Length; i++){
+                if(this.muscles[i].id == Id)
+                    muscle = this.muscles[i];
+            }
+            return muscle;
+        }
+
+        void UpdateOutput(int Id, float value)
+        {
+            Cell cell = GetCell(Id);
+            Muscle muscle = null;
+
+            if(value>3)
+                value = 3;
+            else if(value<-3)
+                value = 1;
+
+            if(cell == null)
+                muscle = GetMuscle(Id);
+                
+            if(cell != null)
+                cell.rb.drag = value*8;
+            else if(muscle != null)
+                muscle.muscle.distance = value;
+            else
+                Debug.Log("Didnt find any output... fuck");
+        }
+
+        void WakeUp(){
+            foreach(Cell cell in cells){
+                cell.rb.WakeUp();
+            }
+        }
+
+        void UpdateInputs(){
+            int Icount = this.nnet.nodes[0].Length;
+            for(int i = 0; i<Icount-1; i++){
+
+            }
+            this.nnet.nodes[0][Icount-1] = Mathf.Sin(Time.time);
+        }
+
+        void UpdateOutputs(){
+            for(int i = 0; i<this.outputs; i++){
+                float outputValue = this.nnet.nodes[this.nnet.nodes.Length-1][i]*3;
+                UpdateOutput(i, outputValue);
+            }
+        }
+
+        public void Update(){
+            WakeUp();
+            UpdateInputs();
+            this.nnet.UpdateNodes();
+            UpdateOutputs();
         }
     }
 
     // Start is called before the first frame update
     void Start()
     {
-        int[][] con = new int[3][];
-        con[0] = new int[]{};
-        con[1] = new int[]{0, 2};
-        con[2] = new int[]{};
-        Org org = new Org(3, con, new Nnet(2, 2, 5, 5), "first");
+        int[][] con = new int[2][];
+        con[0] = new int[]{2};
+        con[1] = new int[]{};
+        Org org = new Org(2, con, new Nnet(1, 1, 1, 3), "first");
+        org.nnet.MutateWeights(5, 0.05f);
+        org.body.AddComponent<OrgController>();
+        org.body.SendMessage("GetOrg", org);
+        Destroy(gameObject);
     }
 
     // Update is called once per frame
---
+++ Assets/scripts/OrgController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OrgController : MonoBehaviour
+{
+    
+    CreateOrganism.Org me;
+    void GetOrg(CreateOrganism.Org org){
+        me = org;
+        me.nnet.weights[0][0, 0] = 1f;
+        me.nnet.weights[1][0, 0] = -10f;
+        me.nnet.weights[1][1, 0] = 3f;
+        me.nnet.weights[1][2, 0] = 10f;
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        me.Update();
+    }
+}
